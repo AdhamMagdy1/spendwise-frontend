63d58c8242216d12845eeba7bf14d615ec01e49a
Adham Magdy <adhammagdy614@gmail.com>
2023-10-03 16:41:32 +0300

    added folder sturcure and global styles and added the app entry structre and styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
-import './App.css';
+import './assets/styles/App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SpendWise
+        <h1 className="H1 LogoText">SpendWise</h1>
+        <p className="P">
+          <span>"</span>Empowering Budgets,
+          <br /> Simplifying Savings!<span>"</span>
         </p>
-        <a
-          className="App-link"
-          href="https://www.adhammagdy.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Adham Magdy
-        </a>
       </header>
+      <div className="actionButtons">
+        <button className="signup H2">create account</button>
+        <button className="login H2">login to account</button>
+      </div>
     </div>
   );
 }
