cd4dc3771bd4909d5d50f295599a1bea8d29b4ae
Adham Magdy <adhammagdy614@gmail.com>
2023-10-03 17:39:56 +0300

    added the routers

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
+import React from 'react';
 import logo from './logo.svg';
 import './assets/styles/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <header className="App-header">
-        <h1 className="H1 LogoText">SpendWise</h1>
-        <p className="P">
-          <span>"</span>Empowering Budgets,
-          <br /> Simplifying Savings!<span>"</span>
-        </p>
-      </header>
-      <div className="actionButtons">
-        <button className="signup H2">create account</button>
-        <button className="login H2">login to account</button>
+    <Router>
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+          <h1 className="H1 LogoText">SpendWise</h1>
+          <p className="P">
+            "Empowering Budgets,
+            <br /> Simplifying Savings!"
+          </p>
+        </header>
+        <div className="actionButtons">
+          <Link to="/signup">
+            <button className="signup H2">Create Account</button>
+          </Link>
+          <Link to="/login">
+            <button className="login H2">Login to Account</button>
+          </Link>
+        </div>
       </div>
-    </div>
+
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
