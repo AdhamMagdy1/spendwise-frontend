af4ad2615a14c46dfc5de1796535a4ae7fa5d05e
Adham Magdy <adhammagdy614@gmail.com>
2023-10-03 15:17:06 +0300

    intial repo and added logo

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          SpendWise
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.adhammagdy.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Adham Magdy
         </a>
       </header>
     </div>
