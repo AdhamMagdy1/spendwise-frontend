de632bcb66e8476dcec642471bdbea61d205ab30
Adham Magdy <adhammagdy614@gmail.com>
2023-10-04 13:30:46 +0300

    added the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../assets/styles/components/Footer.css';
+
+function Footer() {
+  return (
+    <div>
+      <p className='footer'>
+        Developed and Desinged by <br />
+        <a href="https://www.adhammagdy.com">Adham Magdy</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,26 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../assets/styles/App.css';
+import Footer from '../components/Footer';
 function Home() {
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <header className="App-header">
-        <h1 className="H1 LogoText">SpendWise</h1>
-        <p className="P">
-          "Empowering Budgets,
-          <br /> Simplifying Savings!"
-        </p>
-      </header>
-      <div className="actionButtons">
-        <Link to="/signup">
-          <button className="signup H2">Create Account</button>
-        </Link>
-        <Link to="/login">
-          <button className="login H2">Login to Account</button>
-        </Link>
+    <>
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+          <h1 className="H1 LogoText">SpendWise</h1>
+          <p className="P">
+            <span>"</span>Empowering Budgets,
+            <br /> Simplifying Savings!<span>"</span>
+          </p>
+        </header>
+        <div className="actionButtons">
+          <Link to="/signup">
+            <button className="signup H2">Create Account</button>
+          </Link>
+          <Link to="/login">
+            <button className="login H2">Login to Account</button>
+          </Link>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
