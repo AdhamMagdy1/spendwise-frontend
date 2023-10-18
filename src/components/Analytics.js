07ecf2a4b5c7041a6e8d7d1cf301552a74bf5176
Adham Magdy <adhammagdy614@gmail.com>
2023-10-18 15:21:06 +0300

    add the staked bar with green colors

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -291,14 +291,62 @@ function Analytics() {
       datasets: datasets,
     };
   }
+
   function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    const colors = [
+      '#00FFFF',
+      '#7FFFD4',
+      '#454B1B',
+      '#088F8F',
+      '#AAFF00',
+      '#5F9EA0',
+      '#097969',
+      '#AFE1AF',
+      '#DFFF00',
+      '#E4D00A',
+      '#00FFFF',
+      '#023020',
+      '#7DF9FF',
+      '#50C878',
+      '#5F8575',
+      '#4F7942',
+      '#228B22',
+      '#7CFC00',
+      '#008000',
+      '#355E3B',
+      '#00A36C',
+      '#2AAA8A',
+      '#4CBB17',
+      '#90EE90',
+      '#32CD32',
+      '#478778',
+      '#0BDA51',
+      '#98FB98',
+      '#8A9A5B',
+      '#0FFF50',
+      '#ECFFDC',
+      '#808000',
+      '#C1E1C1',
+      '#C9CC3F',
+      '#B4C424',
+      '#93C572',
+      '#96DED1',
+      '#8A9A5B',
+      '#2E8B57',
+      '#9FE2BF',
+      '#009E60',
+      '#00FF7F',
+      '#008080',
+      '#40E0D0',
+      '#C4B454',
+      '#40B5AD',
+      '#40826D',
+    ];
+
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
   }
+
   const thedata = generateChartData(uniqueTagsAndTotalPriceByTag);
   console.log(thedata);
   const barchartData = {
@@ -323,6 +371,11 @@ function Analytics() {
         stacked: true,
       },
     },
+  };
+
+  // Function to toggle the tag type
+  const toggleTagType = () => {
+    setTagType(tagType === 'primary' ? 'secondary' : 'primary');
   };
   useEffect(() => {
     // Save the start and end dates to local storage whenever they change
@@ -382,6 +435,9 @@ function Analytics() {
             </div>
           </>
         )}
+        <button className="switch H3" onClick={toggleTagType}>
+          Switch Tags
+        </button>
       </div>
     </div>
   );
